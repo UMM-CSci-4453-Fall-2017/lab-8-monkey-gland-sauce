5099b6ef0f8a59d0339d2340f066297dea29111d
tszhonglau <lauxx265@morris.umn.edu>
2017-11-07 22:32:45 -0600

    Made changes to click.js, express.js, index.html, added ability to make tables, as well as delete record when pressing the item on the table

--- express.js
+++ express.js
@@ -8,12 +8,29 @@ app = express(),
 port = process.env.PORT || 1337;
 
 var buttons = [];
+var list = [];
 var databases = [];
 
 var getButtonsInfo = function(dbf){
   var sql = "SELECT * FROM " + credentials.user + ".till_buttons";
   queryResults = dbf.query(mysql.format(sql));
   return(queryResults);
+}
+
+var fillInButtonsArray = function(result){
+  buttons = result;
+  return(buttons);
+}
+
+var getTransactionData = function(dbf){
+  var sql = "SELECT * FROM " + credentials.user + ".transaction";
+  queryResults = dbf.query(mysql.format(sql));
+  return(queryResults);
+}
+
+var fillInList = function(result){
+  list = result;
+  return(list);
 }
 
 var insertIntoTransaction = function(dbf, sql){
@@ -24,15 +41,9 @@ var truncateTable = function(dbf, sql){
   dbf.query(mysql.format(sql));
 }
 
-var fillInButtonsArray = function(result){
-  buttons = result;
-  return(buttons);
-}
-
 var getCurrentUser = function(){
   return(credentials.user);
 }
-
 
 app.use(express.static(__dirname + '/public')); //Serves the web pages
 app.get("/buttons",function(req,res){
@@ -48,30 +59,46 @@ app.post("/void",function(req,res){
   var query = truncateTable(dbf, sql);
 });
 
-app.get("/user", function(req, res){
-  res.send(getCurrentUser());
+app.get("/list",function(req,res){
+  var query = getTransactionData(dbf)
+  .then(fillInList)
+  .then(function (list) {
+    res.send(list);})
+  .catch(function(err){console.log("DANGER:",err)});
 });
+
+// app.get("/user", function(req, res){
+//   console.log("helloo");
+//   res.send(credentials.user);
+// });
 
 app.post("/click",function(req,res){
   var id = req.param('id');
   var sql;
   if (id == '1'){
-    sql = 'INSERT INTO ' + credentials.user + '.transaction(name, amount, cost) values ("hot dogs", 1, 2.50)';
+    sql = 'insert into ' + credentials.user + '.transaction values (1, "hot dogs", 1, 0.78) ' +
+    'on duplicate key update amount = amount + 1, cost = cost + 0.78';
   }
-
   if (id == '2'){
-    sql = 'INSERT INTO ' + credentials.user + '.transaction(name, amount, cost) values ("dabu dabu", 1, 1.50)';
+    sql = 'insert into ' + credentials.user + '.transaction values (2, "dabu dabu", 1, 1.78) ' +
+    'on duplicate key update amount = amount + 1, cost = cost + 1.78';
   }
-
   if (id == '3'){
-    sql = 'INSERT INTO ' + credentials.user + '.transaction(name, amount, cost) values ("anchovy paste", 1, 3.50)';
+    sql = 'insert into ' + credentials.user + '.transaction values (3, "anchovy paste", 1, 2.50) ' +
+    'on duplicate key update amount = amount + 1, cost = cost + 2.50';
   }
-
   if (id == '4'){
-    sql = 'INSERT INTO ' + credentials.user + '.transaction(name, amount, cost) values ("tabasco", 1, 4.50)';
+    sql = 'insert into ' + credentials.user + '.transaction values (4, "tabasco", 1, 1.25) ' +
+    'on duplicate key update amount = amount + 1, cost = cost + 1.25';
   }
-
   var query = insertIntoTransaction(dbf, sql);
   //.catch(function(err){console.log("DANGER:",err)});
-  });
+});
+
+app.post("/delete", function(req,res){
+  var id = req.param('id');
+  var sql = 'DELETE FROM ' + credentials.user + '.transaction where id = ' + id;
+  var query = insertIntoTransaction(dbf, sql);
+});
+
 app.listen(port);
--- public/click.js
+++ public/click.js
@@ -1,69 +1,133 @@
-angular.module('buttons',[])
-  .controller('buttonCtrl',ButtonCtrl)
-  .factory('buttonApi',buttonApi)
-  .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
+  angular.module('buttons',[])
+    .controller('buttonCtrl',ButtonCtrl)
+    .factory('buttonApi',buttonApi)
+    .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
 
-function ButtonCtrl($scope,buttonApi){
-   $scope.buttons=[]; //Initially all was still
-   $scope.errorMessage='';
-   $scope.isLoading=isLoading;
-   $scope.refreshButtons=refreshButtons;
-   $scope.buttonClick=buttonClick;
-   $scope.voidClick=voidClick;
-
-   var loading = false;
-
-   function isLoading(){
-    return loading;
-   }
-  function refreshButtons(){
-    loading=true;
-    $scope.errorMessage='';
-    buttonApi.getButtons()
-      .success(function(data){
-         $scope.buttons=data;
-         loading=false;
-      })
-      .error(function () {
-          $scope.errorMessage="Unable to load Buttons:  Database request failed";
-          loading=false;
-      });
- }
-  function buttonClick($event){
+  function ButtonCtrl($scope,buttonApi){
+     $scope.buttons=[]; //Initially all was still
+     $scope.list=[];
+     //$scope.user=[];
      $scope.errorMessage='';
-     //console.log($event.target.id);
-     buttonApi.clickButton(event.target.id)
-        .success(function(){})
-        .error(function(){$scope.errorMessage="Unable click";});
-  }
+     $scope.deleteItem=deleteItem;
+     //$scope.currentUser='';
+     $scope.isLoading=isLoading;
+     $scope.refreshButtons=refreshButtons;
+     $scope.refreshList=refreshList;
+     $scope.buttonClick=buttonClick;
+     $scope.voidClick=voidClick;
+     //$scope.getUser=getUser;
 
-  function voidClick($event) {
-    console.log("should be here");
-    $scope.errorMessage='';
-    //console.log($event.target.id);
-    buttonApi.voidButton()
-       .success(function(){})
-       .error(function(){$scope.errorMessage="Unable to void this transaction";});
-  }
+     var loading = false;
 
-  refreshButtons();  //make sure the buttons are loaded
-}
+     function isLoading(){
+      return loading;
+     }
 
-function buttonApi($http,apiUrl){
-  return{
-    getButtons: function(){
-      var url = apiUrl + '/buttons';
-      return $http.get(url);
-    },
-    clickButton: function(id){
-      var url = apiUrl+'/click?id='+id;
-      console.log("Attempting with "+url);
-      return $http.post(url); // Easy enough to do this way
-    },
-    voidButton: function(){
-      var url = apiUrl + '/void';
-      console.log("Attempting with "+url);
-      return $http.post(url);
+    //  function getUser(){
+    //    loading=true;
+    //    $scope.currentUser='';
+    //    buttonApi.getUser()
+    //     .success(function(data) {
+    //       $scope.currentUser=data;
+    //       loading=false;
+    //     })
+    //     .error(function () {
+    //         $scope.errorMessage="Unable to load Buttons:  Database request failed";
+    //         loading=false;
+    //     });
+    //  }
+    function deleteItem(){
+      $scope.errorMessage='';
+      //console.log($event.target.id);
+      buttonApi.deleteItem(event.target.id)
+         .success(function(){
+           refreshList();
+         })
+         .error(function(){$scope.errorMessage="Unable click";});
     }
- };
-}
+
+    function refreshButtons(){
+      loading=true;
+      $scope.errorMessage='';
+      buttonApi.getButtons()
+        .success(function(data){
+           $scope.buttons=data;
+           loading=false;
+        })
+        .error(function () {
+            $scope.errorMessage="Unable to load Buttons:  Database request failed";
+            loading=false;
+        });
+   }
+
+   function refreshList(){
+     loading=true;
+     $scope.errorMessage='';
+     buttonApi.getList()
+       .success(function(data){
+          $scope.list=data;
+          loading=false;
+       })
+       .error(function () {
+           $scope.errorMessage="Unable to load Buttons:  Database request failed";
+           loading=false;
+       });
+  }
+
+    function buttonClick($event){
+       $scope.errorMessage='';
+       //console.log($event.target.id);
+       buttonApi.clickButton(event.target.id)
+          .success(function(){
+            refreshList();
+          })
+          .error(function(){$scope.errorMessage="Unable click";});
+    }
+
+    function voidClick($event){
+      $scope.errorMessage='';
+      //console.log($event.target.id);
+      buttonApi.voidButton()
+         .success(function(){
+           refreshList();
+         })
+         .error(function(){$scope.errorMessage="Unable to void this transaction";});
+    }
+
+    refreshButtons();  //make sure the buttons are loaded
+    refreshList();
+  }
+
+  function buttonApi($http,apiUrl){
+    return{
+      getButtons: function(){
+        var url = apiUrl + '/buttons';
+        return $http.get(url);
+      },
+      clickButton: function(id){
+        var url = apiUrl+'/click?id='+id;
+        console.log("Attempting with "+url);
+        return $http.post(url); // Easy enough to do this way
+      },
+      voidButton: function(){
+        var url = apiUrl + '/void';
+        console.log("Attempting with "+url);
+        return $http.post(url);
+      },
+      getList: function(){
+        var url = apiUrl + '/list';
+        console.log("Attempting with " + url);
+        return $http.get(url);
+      },
+      getUser: function(){
+        var url = apiUrl + '/user';
+        console.log("Attempting with " + url);
+        return $http.get(url);
+      },
+      deleteItem: function(id){
+        var url = apiUrl+'/delete?id='+id;
+        console.log("Attempting with "+url);
+        return $http.post(url);
+      }
+   };
+  }
